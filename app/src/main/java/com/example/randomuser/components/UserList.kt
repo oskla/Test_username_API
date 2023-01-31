699143b124aab8d4a55fb6a54fe7d9b7ff9fd4d4
Oskar Larsson <90769758+oskla@users.noreply.github.com>
2023-01-31 13:04:55 +0100

    klint format

--- app/src/main/java/com/example/randomuser/components/UserList.kt
+++ app/src/main/java/com/example/randomuser/components/UserList.kt
@@ -8,12 +8,10 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.randomuser.data.UserItemData
 import com.example.randomuser.data.usersData
 import com.example.randomuser.exampledata.exampleResult
 import com.example.randomuser.exampledata.exampleResults
@@ -29,8 +27,6 @@ fun UsersList(
     usersData: Result?
 ) {
     LazyColumn(verticalArrangement = Arrangement.spacedBy(16.dp)) {
-
-
         if (usersData != null) {
             itemsIndexed(items = usersData.results) { _, item ->
                 UserItem(
@@ -39,7 +35,7 @@ fun UsersList(
                     cardPaddingHorizontal = 0.dp,
                     detailsState = detailsState,
                     userListState = userListState,
-                    currentUser = currentUser,
+                    currentUser = currentUser
                 )
             }
         }
@@ -60,7 +56,7 @@ fun UsersListPreview() {
             usersData = exampleResult,
             detailsState = detailsState,
             userListState = userListState,
-            currentUser = mutableStateOf(exampleResults),
+            currentUser = mutableStateOf(exampleResults)
         )
     }
 }
