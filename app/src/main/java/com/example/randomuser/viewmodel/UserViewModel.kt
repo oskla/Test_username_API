cfb5cae88fa4ce4e947e7d3280c566f20ca414e3
Oskar Larsson <90769758+oskla@users.noreply.github.com>
2023-02-01 10:00:46 +0100

    recovering stuff i messed up in merge conflict

--- app/src/main/java/com/example/randomuser/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/randomuser/viewmodel/UserViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.randomuser.viewmodel
 
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.randomuser.data.Filter
 import com.example.randomuser.data.UserItemData
 import com.example.randomuser.repo.Repo
 import kotlinx.coroutines.Dispatchers
@@ -27,7 +29,20 @@ class UserViewModel : ViewModel() {
         }
     }
 
-    fun setToCurrentUser(user: UserItemData) {
-        currentUser = user
+    fun getFilteredUsers(selectedFilter: MutableState<Filter?>) {
+        // If filter is deselected
+        if (selectedFilter.value?.selected == false) {
+
+
+            resultsResponse?.results?.let { users.addAll(it) }
+            return
+        }
+
+        var filteredUsers = users.filter { it.gender.lowercase() == selectedFilter.value?.text?.lowercase() }
+        val filteredUsers = users.filter { it.gender.lowercase() == selectedFilter.value?.text?.lowercase() }
+        users.clear()
+        users.addAll(filteredUsers)
     }
+}
+
 }
